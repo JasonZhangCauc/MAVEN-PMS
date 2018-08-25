cff0e13f90486b36cad8e7b121a72ae60440a51a
JasonZhangCauc <jmzhang_15_cauc@163.com>
2018-08-25 22:05:06 +0800

    Jasonzhang

--- PMS Maven Webapp/src/main/java/com/ps/service/contractmanage/IContractManageService.java
+++ PMS Maven Webapp/src/main/java/com/ps/service/contractmanage/IContractManageService.java
@@ -6,50 +6,50 @@ import com.ps.result.contractmanage.ContractVo;
 
 /**
  * @Description:    合同维护接口类
- * @author:         raomingyi
- * @date:           2018-03-13
+ * @author:         Jasonzhang
+ * @date:           2018-08-13
  */
 public interface IContractManageService {
 
 	/**
 	 * @Description: 查询合同信息
-	 * @author:      raomingyi
+	 * @author:      Jasonzhang
 	 * @param:       ContractVo
 	 * @return:      List<ContractVo>数据列表   
-	 * @date:        2018-03-13
+	 * @date:        2018-08-13
 	 */
 	List<ContractVo> queryContractInfoByParam(ContractVo param);
 	
 	/**
 	 * @Description: 根据ID查询合同数据信息
-	 * @author:      raomingyi
+	 * @author:      Jasonzhang
 	 * @param:       id
 	 * @return:      ContractManageVo数据   
-	 * @date:        2018-03-13
+	 * @date:        2018-08-13
 	 */
 	ContractVo queryContractInfoById(String id);
 	
 	/**
 	 * @Description: 保存合同信息
-	 * @author:      raomingyi
+	 * @author:      Jasonzhang
 	 * @param:       ContractVo   
-	 * @date:        2018-03-13
+	 * @date:        2018-08-13
 	 */
 	void saveContract(ContractVo vo);
 	
 	/**
 	 * @Description: 根据ID删除合同信息
-	 * @author:      raomingyi
+	 * @author:      Jasonzhang
 	 * @param:       id submmitContract
-	 * @date:        2018-03-13
+	 * @date:        2018-08-13
 	 */
 	void deleteContractById(String id);
 	
 	/**
 	 * @Description: 根据提交合同
-	 * @author:      raomingyi
+	 * @author:      Jasonzhang
 	 * @param:       id submmitContract
-	 * @date:        2018-03-13
+	 * @date:        2018-08-13
 	 */
 	void updateContractSendStatus(String id);
 }
