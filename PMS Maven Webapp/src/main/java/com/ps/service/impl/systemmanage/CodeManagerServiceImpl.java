2328f4657e843beeb429aa941282c2e55529c844
JasonZhangCauc <hasp_Jason@163.com>
2018-09-11 23:14:55 +0800

    0911
    Signed-off-by: JasonZhangCauc <hasp_Jason@163.com>

--- PMS Maven Webapp/src/main/java/com/ps/service/impl/systemmanage/CodeManagerServiceImpl.java
+++ PMS Maven Webapp/src/main/java/com/ps/service/impl/systemmanage/CodeManagerServiceImpl.java
@@ -136,4 +136,167 @@ public class CodeManagerServiceImpl extends BaseServiceImpl implements ICodeMana
 		List<CodeVo> dataList = this.codeManagerDao.queryParentCode(codeVo);
 		return dataList;
 	}
+
+	//计算当前用户所拥有的岗位
+	public String returnJobs(String ManagerDiff){
+		String [] position = this.getLoginUser().getUserPosition().split(",");
+		String positions = "";
+		//统计需要查询的角色
+		for (int i = 0; i < position.length; i++) {
+			if(position[i].equals(GlobalConstants.ONE)){
+				positions+= "," + GlobalConstants.ONE;
+			}else if(position[i].equals(GlobalConstants.TWO)){
+				positions+= "," + GlobalConstants.TWO;
+			}else if(position[i].equals(GlobalConstants.THREE)){
+				positions+= "," + GlobalConstants.THREE;
+			}else if(position[i].equals(GlobalConstants.FOUR)){
+				positions+= "," + GlobalConstants.FOUR;
+			}
+		}
+		String retVal = "";
+		if(!ManagerDiff.equals("")){
+			if(!positions.equals("")){
+				positions = positions.substring(1);
+				//根据岗位赋予传查询值
+				String [] ids = positions.split(",");
+				for (int j = 0; j < ids.length; j++) {
+					if(ids[j].equals(GlobalConstants.ONE)){
+						//当状态等于三的时候给验证通过值
+						if(!StringUtils.isBlank(ManagerDiff)){
+							if(ManagerDiff.equals(GlobalConstants.THREE)){
+								if(retVal.indexOf(GlobalConstants.THREE) == -1){
+									retVal += ","+GlobalConstants.THREE+","+GlobalConstants.FOUR+","+GlobalConstants.FIVE;
+								}else{
+									retVal += ","+GlobalConstants.FOUR+","+GlobalConstants.FIVE;
+								}
+							}else{
+								if(ManagerDiff.equals(GlobalConstants.FIVE)){
+									if(retVal.indexOf(ManagerDiff) == -1){
+										retVal += ","+GlobalConstants.FIVE;
+									}
+									if(retVal.indexOf(GlobalConstants.FOUR) == -1){
+										retVal += ","+GlobalConstants.FOUR;
+									}
+								}else if(ManagerDiff.equals(GlobalConstants.FOUR)){
+									retVal += ","+GlobalConstants.FOUR+","+GlobalConstants.SIX;
+								}
+							}
+						}
+					}else if(ids[j].equals(GlobalConstants.TWO)){
+						//当状态等于二的时候给验证通过值
+						if(!StringUtils.isBlank(ManagerDiff)){
+							if(ManagerDiff.equals(GlobalConstants.TWO)){
+								if(retVal.indexOf(GlobalConstants.THREE) == -1){
+									retVal += ","+GlobalConstants.THREE;
+								}
+								if(retVal.indexOf(GlobalConstants.TWO) == -1){
+									retVal += ","+GlobalConstants.TWO;
+								}
+							}else{
+								if(retVal.indexOf(ManagerDiff) == -1){
+									retVal += ","+ManagerDiff;
+								}
+							}
+						}
+					}else if(ids[j].equals(GlobalConstants.THREE)){
+						if(!StringUtils.isBlank(ManagerDiff)){
+							if(ManagerDiff.equals(GlobalConstants.FOUR)){
+								retVal += ","+GlobalConstants.SIX;
+							}else{
+								if(retVal.indexOf(ManagerDiff) == -1){
+									retVal += ","+ManagerDiff;
+								}
+								//retVal += ","+GlobalConstants.ONE;
+							}
+						}else{
+							if(retVal.indexOf(ManagerDiff) == -1){
+								retVal += ","+ManagerDiff;
+							}
+							//retVal += ","+GlobalConstants.ONE;
+						}
+					}else if(ids[j].equals(GlobalConstants.FOUR)){
+						if(!StringUtils.isBlank(ManagerDiff)){
+							if(ManagerDiff.equals(GlobalConstants.ONE)){
+								if(retVal.indexOf(GlobalConstants.ONE) == -1){
+									retVal += ","+GlobalConstants.ONE;
+								}
+								if(retVal.indexOf(GlobalConstants.TWO) == -1){
+									retVal += ","+GlobalConstants.TWO;
+								}
+							}
+						}
+					}
+				}
+				if(!retVal.equals("")){
+					retVal = retVal.substring(1);
+				}
+			}
+		}else{
+			retVal += ","+GlobalConstants.ONE;
+		}
+		return retVal;
+	}
+	
+	@Override
+	public Map<String, List<CodeVo>> initCodeInfoPlanState(CodeVo vo) {
+		Map<String, List<CodeVo>> treeMap = new HashMap<String, List<CodeVo>>();
+		try {
+			vo.setCodeValueStr(returnJobs(vo.getManagerDiff()));//返回状态
+			List<CodeVo> all = this.codeManagerDao.initCodeInfoPlanState(vo);
+			List<CodeVo> children = null;
+			if (all!=null) {
+				for (CodeVo basicVo : all) {
+					if(treeMap.containsKey(basicVo.getCodeType())){
+						treeMap.get(basicVo.getCodeType()).add(basicVo);
+					}else{
+						children = new ArrayList<CodeVo>();
+						children.add(basicVo);
+						treeMap.put(basicVo.getCodeType(), children);
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new BussinessException(new BussinessExceptionBean("exception.error.syserror"), e);
+		}
+		return treeMap;
+	}
+
+	@Override
+	public List<CodeVo> initDocumentCode(CodeVo vo) {
+		List<CodeVo> list = new ArrayList<CodeVo>();
+		if(!StringUtils.isBlank(vo.getCodeTypeStr())){
+			if(vo.getCodeTypeStr().equals(GlobalConstants.ONE)){
+				vo.setCodeType("documentClass01");
+			}else if(vo.getCodeTypeStr().equals(GlobalConstants.TWO)){
+				vo.setCodeType("documentClass02");
+			}
+			list = codeManagerDao.initDocumentCode(vo);
+		}
+		return list;
+	}
+	
+	
+	@Override
+	public List<CodeVo> initTeachMouldCode(CodeVo vo) {
+		List<CodeVo> list = new ArrayList<CodeVo>();
+		if(!StringUtils.isBlank(vo.getCodeTypeStr())){
+			if(vo.getCodeTypeStr().equals(GlobalConstants.ONE)){
+				vo.setCodeType("teachMouldType03");
+			}else if(vo.getCodeTypeStr().equals(GlobalConstants.TWO)){
+				vo.setCodeType("teachMouldType02");
+			}
+			else if(vo.getCodeTypeStr().equals(GlobalConstants.ZERO)){
+				vo.setCodeType("teachMouldType01");
+			}
+			list = codeManagerDao.initTeachMouldCode(vo);
+		}
+		return list;
+	}
+
+	@Override
+	public List<CodeVo> overStatus(CodeVo vo) {
+		vo.setStatusStrs(GlobalConstants.ONE+","+GlobalConstants.TWO);
+		List<CodeVo> list = codeManagerDao.overStatus(vo);
+		return list;
+	}
 }
